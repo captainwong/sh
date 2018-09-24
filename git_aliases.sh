1756b9f6d5ae40a74d4cfbc951761b0746d63cbd
win10_i5-4590 <1281261856@qq.com>
2018-09-24 23:26:31 +0800

    add git_aliases.sh

---
+++ git_aliases.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git config --global alias.st 'status'
+git config --global alias.aa 'add --all'
+git config --global alias.cm 'commit -m'
+git config --global alias.cb 'checkout -b'
+git config --global alias.last 'log -1'
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.po 'push origin master'
+

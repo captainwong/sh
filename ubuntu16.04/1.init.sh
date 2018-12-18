368f65713df949b0bfc753f1f385cce8c0a3a911
win10_i5-4590 <1281261856@qq.com>
2018-12-18 17:18:34 +0800

    update

--- ubuntu16.04/1.init.sh
+++ ubuntu16.04/1.init.sh
@@ -4,4 +4,5 @@ sudo mv /etc/apt/sources.list /etc/apt/sources.list.bk
 sudo cp ./apt.sources.list /etc/apt/sources.list
 sudo apt-get update -y
 
-sudo bash tools/install.sh
+cd tools
+sudo ./install.sh

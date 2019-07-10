98fdc693daeb9f65dbcfeb71f357cde27f3bcd12
i7-8700 <1281261856@qq.com>
2019-07-10 18:03:02 +0800

    update

--- git/git_aliases.sh
+++ git/git_aliases.sh
@@ -6,5 +6,5 @@ git config --global alias.cm 'commit -m'
 git config --global alias.cb 'checkout -b'
 git config --global alias.last 'log -1'
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-git config --global alias.po 'push origin master'
+git config --global alias.po 'push origin'
 git config credential.helper 'cache --timeout=3600'

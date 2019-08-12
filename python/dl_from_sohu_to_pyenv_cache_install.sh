f3fe5f77faca8b38256bd929555de94f735ee62c
i7-8700 <1281261856@qq.com>
2019-08-13 01:29:37 +0800

    update

--- python/dl_from_sohu_to_pyenv_cache_install.sh
+++ python/dl_from_sohu_to_pyenv_cache_install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# https://github.com/pyenv/pyenv/wiki/Common-build-problems
+echo "On Debian stretch (and Ubuntu bionic), libssl-dev is OpenSSL 1.1.x, "
+echo "but support for that was only added in Python 2.7.13, 3.5.3 and 3.6.0. "
+echo "To install earlier versions, you need to replace libssl-dev with libssl1.0-dev. "
+echo "This is being tracked in https://github.com/pyenv/pyenv/issues/945."
+
 wget http://mirrors.sohu.com/python/$1/Python-$1.tar.xz -P ~/.pyenv/cache/
 pyenv install $1
---
+++ python/use_aliyun_mirror.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p ~/.pip
+cp ./aliyun_pip.conf ~/.pip/pip.conf

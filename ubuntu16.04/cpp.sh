2766fb9dc08acae5d3043b0d75498d1736abd282
i7-8700 <1281261856@qq.com>
2019-08-16 18:07:11 +0800

    update

--- ubuntu16.04/cpp.sh
+++ ubuntu16.04/cpp.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install -y build-essential libboost-all-dev libcrypto++9v5 libcrypto++9v5-dbg libcrypto++-dev libssl-dev libmysqld-dev libmysqlcppconn-dev 
+sudo apt-get install -y build-essential libboost-all-dev \
+    libcrypto++9v5 libcrypto++9v5-dbg libcrypto++-dev \
+    libssl-dev libmysqld-dev libmysqlcppconn-dev \
+    libcurl4-openssl-dev g++ gcc libtool-bin autoconf \
+    pkg-config
